4e42410feb291e30f0b88b13695544fc40173538
Karel Velička <tucna@tucna>
2022-02-17 00:54:19 +0100

    added argparse, added ascii list option, added resizing, added save to file

--- README.md
+++ README.md
@@ -1,14 +1,61 @@
 # i2a
 
-Image to Ascii convertor made in C.
+![icon](icon.png)Image to Ascii convertor made in C. The project is made with the help of [stb library](https://github.com/nothings/stb). 
 
 
 
-## Sample
+## Usage
+
+```
+usage: i2a [file] [-h] [-v] [-a] [-f] [-l] [-m]
+
+required arguments:
+ [file]  file path of your image
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --version  show current version
+  -a, --about    show about page
+  -f, --file     Save result into the file.
+  -l, --less     Use ASCII list with less characters -> higher contrast.
+  -m , --more    Use ASCII list with more characters -> better quality, worse contrast.
+```
 
 
 
-| original                         | Ascii                             |
-| -------------------------------- | --------------------------------- |
-| ![man_original](samples/man.jpg) | ![man_converted](samples/man.png) |
+## Screenshots
 
+
+
+| Original                                      | Ascii                                             |
+| --------------------------------------------- | ------------------------------------------------- |
+| ![gandalf original](samples/gandalf_pink.jpg) | ![gandalf converted](samples/gandalf.png)         |
+| ![github original](samples/github.jpg)        | ![github comverted](samples/github_converted.png) |
+
+
+
+## TODO
+
+- [x] show image in terminal
+
+- [x] resize by terminal window
+
+- [x] jpg support
+
+- [ ] png support
+
+- [ ] bmp support
+
+- [ ] gif support
+
+- [ ] video support
+
+- [x] save to file
+
+- [x] add argparse
+
+- [x] ASCII list selection
+
+- [ ] make code readable :(
+
+  
--- i2a.c
+++ i2a.c
@@ -4,43 +4,82 @@
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <regex.h>
 
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+
 #include "stb/stb_image.h"
+#include "stb/stb_image_resize.h"
 
+#define ASCII_less "M$&#x=+;-'`. "
 #define ASCII "M@#W$BG5E20Tbca?1!;:+=-,._` "
-#define ASCII_full "M$@WB%8&#Z0OQLCXYUJhkbdpqoawmzcvunxrjft/\\?*-_+~<>1i!lI|;:,\"^`'. "
-#define ASCII_low "M$&#x=+;-'`. "
-#define ASCII_real "███▓▓▒░░  "
+#define ASCII_more "M$@WB%8&#Z0OQLCXYUJhkbdpqoawmzcvunxrjft/\\?*-_+~<>1i!lI|;:,\"^`'. "
 
 
+// argparse info
+//
+int ascii_value;
+int file_option = 0;
+const char *argparse_version = "Version 0.0.2 (16.02.2022)";
+static char doc[] = "Image to Ascii convertor made in C.\n    ---> https://github.com/tucnakomet1/i2a\n    ---> tucnakomet@gmail.com";
+
+
+// structure 
+//
 typedef struct {
   int width;
   int height;
+  int channels;
   unsigned int** data;
 } Image;
 
-// all functions and classes
 Image loadImage(const char *file_path);
 int load(const char *file);
-int ascii_list_size = sizeof(ASCII_full) - 2;
+int count_average(int r, int g, int b);
 
-// main image class → load image and define data
+
+
+// main Image class → load image and define data
+//
 Image loadImage(const char *file_path) {
-    // load image
     int width, height, channels;
     unsigned char *data = stbi_load(file_path, &width, &height, &channels, 0);
 
-    // image resolution counter
+    // get terminal window size
+    struct winsize w;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w); 
+    
+    int window_width = w.ws_col;
+
+    // resizing the image
+    if (window_width < width) {
+        int window_height = round(height/(width / (float)window_width));
+        //printf ("Resoluition %dx%d 'll be resized to %dx%d ", width, height, window_width, window_height); //
+
+        Image resolution = {width, height};
+        resolution.data = malloc(height * sizeof(int*));
+
+        unsigned char * resized_data = malloc(3 * sizeof(unsigned char) * window_width * window_height);
+
+        int r = stbir_resize_uint8(data, width, height, 0, resized_data, window_width, window_height, 0, 3);
+
+        if (r == 0) { perror("Resizing Error:"); }
+
+        data = resized_data;
+        width = window_width;
+        height = window_height;
+    }
+
     Image resolution = {width, height};
     resolution.data = malloc(height * sizeof(int*));
 
-    // adding data to [i]
+    // allocating data [i]
     for (int i = 0; i < height; i++) {
         resolution.data[i] = malloc((width * 3) * sizeof(int));
     }
 
-    // adding data to [i][j]
+    // allocating data [i][j]
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width * 3; i++) {
             resolution.data[j][i] = data[i + (j * width * 3)];
@@ -51,18 +90,29 @@ Image loadImage(const char *file_path) {
     return resolution;
 }
 
+
+// counting average of RGB
+//
 int count_average(int r, int g, int b) {
     double simple_avg = (r + g + b)/3;
-    double rgb_piece = 255.0 / ascii_list_size;
-    double devided = simple_avg / rgb_piece;
-    int avg = round(devided);
+    int ascii_list_size;
 
-    //printf("\nsimple_avg: %f, rgb_piece: %f, devided: %f, avg: %d\n", simple_avg, rgb_piece, devided, avg);
+    if (ascii_value == 0) {
+        ascii_list_size = sizeof(ASCII) - 2;
+    } else if (ascii_value == 1) {
+        ascii_list_size = sizeof(ASCII_less) - 2;
+    } else {
+        ascii_list_size = sizeof(ASCII_more) - 2;
+    }
+    double rgb_piece = 255.0 / ascii_list_size;
+    int avg = round(simple_avg / (float)rgb_piece);
 
     return avg;
 }
 
 
+// load function → call Image and print result
+//
 int load(const char *file) {
     Image img = loadImage(file);
 
@@ -70,67 +120,106 @@ int load(const char *file) {
     w = img.width;
     h = img.height;
 
-    printf("\nW:%d, H:%d\n", w, h);
+    //printf("\nW:%d, H:%d\n", w, h); //
 
-    if (w == 0) {
-        return -1;
-    }
+    if (w == 0) { return -1; }
 
+    FILE *fp;
+    if (file_option == 1) { fp = fopen("i2a.txt", "w+"); }
 
     for (int i = 0; i < h; i++) {
-        char str[302] = "";
-
-        for (int j = 0; j < w; j++) {
-            //printf("\nI:%d, J:%d", i, j);
+        for (int j = 0; j < w*3; j+=3){
             
             int r = img.data[i][j];
             int g = img.data[i][j+1];
             int b = img.data[i][j+2];
             
-            //printf("\ntry: R: %d, G: %d, B: %d\n", r, g, b);
+            //printf("\ntry: R: %d, G: %d, B: %d\n", r, g, b); //
 
             int avg = count_average(r, g, b);
-            //int ascii_char = ASCII[avg];
-            int ascii_char = ASCII_full[avg];
+            int ascii_char;
 
-            strncat(str, &ascii_char, 1);
+            if (ascii_value == 0) {
+                ascii_char = ASCII[avg];
+            } else if (ascii_value == 1) {
+                ascii_char = ASCII_less[avg];
+            } else {
+                ascii_char = ASCII_more[avg];
+            }
+
+            printf("%c", ascii_char);
+            if (file_option == 1) { fprintf(fp, "%c", ascii_char); }
+
         }
-        printf("%s\n", str);
+        printf("\r\n");
+        if (file_option == 1) { fprintf(fp, "\r\n"); }
     }
+    if (file_option == 1) { fclose(fp); }
 
     return 0;
-
 }
 
-int main() {
 
-    // check for path
+// main function → check for input arguments and call load function
+//
+
+int main(int argc, const char **argv) {
+    
+    // argparse
+    regex_t regex;
     const char *file;
     struct stat sb;
+    int reg_val;
 
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    if (argc < 2) {
+        printf("Please enter file path...\n [-h] for help\n");
+        return -1;
+    } else {
+        for (int i = 1; i < argc; i++) {
 
-    printf ("lines %d\n", w.ws_row);
-    printf ("columns %d\n", w.ws_col);
+            reg_val = regcomp( &regex, "[.(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)]", 0);
+            reg_val = regexec( &regex, argv[1], 0, NULL, 0);
 
-    //file = "/home/tucna/Plocha/i2a/red.jpg";
-    //file = "/home/tucna/Plocha/i2a/black.png";
-    //file = "/home/tucna/Plocha/i2a/white.png";
-    //file = "/home/tucna/Plocha/square.jpg";
-    file = "/home/tucna/Plocha/i2a/inputs/circle.jpg";
-    //file = "/home/tucna/Plocha/i2a/inputs/man.jpg";
+            if ((strcmp(argv[i], "-v") == 0) || (strcmp(argv[i], "--version") == 0)&& (i == 1)) {
+                printf("%s\n", argparse_version);
+                return -1;
+            }
+            if ((strcmp(argv[i], "-a") == 0) || (strcmp(argv[i], "--about") == 0) && (i == 1)) {
+                printf("\n%s\n\n%s\n", argparse_version, doc);
+                return -1;
+            }
+            if ((strcmp(argv[i], "-h") == 0) || (strcmp(argv[i], "--help") == 0) && (i == 1)) {
+                printf("\nusage: i2a [file] [-h] [-v] [-a] [-f] [-l] [-m]\n");
+                printf("\nrequired arguments:\n [file]  file path of your image\n");
+                printf("\noptional arguments:\n  -h, --help     show this help message and exit\n  -v, --version  show current version\n  -a, --about    show about page\n  -f, --file     Save result into the file.\n  -l, --less     Use ASCII list with less characters -> higher contrast.\n  -m , --more    Use ASCII list with more characters -> better quality, worse contrast.\n");
+                printf("\n<https://github.com/tucnakomet1/i2a>\n");
+                return -1;
+            }
+            if ((i == 1) && (reg_val == 0)) {
+                ascii_value = 0;
+                file = argv[i];
+            }
+            if ((strcmp(argv[i], "-l") == 0) || (strcmp(argv[i], "--low") == 0) && (i > 1)) {
+                ascii_value = 1;
+            }
+            if ((strcmp(argv[i], "-m") == 0) || (strcmp(argv[i], "--more") == 0) && (i > 1)) {
+                ascii_value = 2;
+            }
+            if ((strcmp(argv[i], "-f") == 0) || (strcmp(argv[i], "--file") == 0) && (i > 1)) {
+                file_option = 1;
+            }
+        }
+    }
+      
 
+    // check for path
+    struct stat sb;
 
-
-
-    //if path → run process; else → input file again
     if (stat(file, &sb) == 0 && S_ISREG(sb.st_mode)) {
         load(file);
     } else {
-        printf("\nWrong file path! Try it again...\n");
-        printf("Path: %s - does not exist", file);
-	main();
+        printf("Path: %s - does not exist!\n\n", file);
+        return -1;
     }
     
     return 0;
@@ -138,11 +227,8 @@ int main() {
 
 
 /* TODO:
- * - resize image by window size
- * - show full image, not just half
- * - add argparse
- * - options of ascii list
- * - save to file
- * - make video
- * - add about → version, url
+ * - png support
+ * - bmp support
+ * - gif support
+ * - video support
  */
