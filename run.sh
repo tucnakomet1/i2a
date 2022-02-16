9030ae43622a93de4e2e5a7c79623e489dd744ed
Tucna <tucnakomet@gmail.com>
2022-02-17 00:54:37 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-clear
-
-/bin/gcc -Wall -Wextra -Wpedantic -g3 -O0 -I/home/tucna/Plocha/i2a -c /home/tucna/Plocha/i2a/i2a.c -o /home/tucna/Plocha/i2a/build/Debug/i2a.o && /bin/gcc -Wall -Wextra -Wpedantic -g3 -O0 -I/home/tucna/Plocha/i2a  /home/tucna/Plocha/i2a/build/Debug/i2a.o -o /home/tucna/Plocha/i2a/build/Debug/outDebug -lm
-
-/home/tucna/Plocha/i2a/build/Debug/./outDebug /home/tucna/Plocha/i2a/samples/github.jpg
